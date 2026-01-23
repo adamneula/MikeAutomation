1d4231a0200cfb02b3c5fc6f099dbeccdfe4f46c
Adam@Genter <adamneula@gmail.com>
2026-01-23 10:32:13 -0800

    Tiebreaking method working for loading reps, goes through primerica account-rep details now but doesn't handle all rep cases

--- MikeScript.py
+++ MikeScript.py
@@ -5,7 +5,7 @@ import os
 # First edit on corp machine
 # Global dictionary to store objects
 reps = {}
-regionMap = {}
+nameToID = {}
 States = {
     'AL', 'AK', 'AZ', 'AR', 'AB', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 
     'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 
@@ -15,7 +15,7 @@ States = {
 }
 
 class Representatives():
-    def __init__(self, name: str, ID: str, state: str, email: str, AE: str, territory: str):
+    def __init__(self, name: str, ID: str, state: str, email: str, AE: str, territory: str, total: float):
         self.Advisor_Name = name
         self.Primary_Rep_ID = ID
         self.True_State = state
@@ -23,10 +23,11 @@ class Representatives():
         self.AE = AE
         self.Territory = territory
         self.Ranking = None
-        self.Sum_of_Total_Assets = None
+        self.Sum_of_Total_Assets = 0
         self.Previous_Month_AUM = None
         self.MoM_Change = None
         self.Dollar_Val_Change = None
+        self.Lifetime_Total = total
         
     def __hash__(self):
         return hash(self.Primary_Rep_ID)
@@ -37,9 +38,12 @@ class Representatives():
         return self.Primary_Rep_ID == other.Primary_Rep_ID
     
     def __str__(self):
-        return f'{self.Advisor_Name} {self.True_State} {self.Primary_Rep_ID} {self.Email} {self.Territory} AE: {self.AE}'
+        return f'{self.Advisor_Name} {self.True_State} {self.Primary_Rep_ID} {self.Email} {self.Territory} AE: {self.AE} Balance: {self.Sum_of_Total_Assets}'
 
-def load_reps_from_csv():
+    def add_account(self, amount):
+        self.Sum_of_Total_Assets += amount
+        
+def load_reps_from_xlsx():
     global reps
     
     # Setup pathing
@@ -57,24 +61,56 @@ def load_reps_from_csv():
     df = df.dropna(subset=['ID'])
     
     for _, row in df.iterrows():
-        full_name = f"{row['First']} {row['Last']}"
+        full_name = f"{str(row['First']).strip()} {str(row['Last']).strip()}"
+        total = float(row['LifeTime'])
+        if full_name.lower() in reps:
+            if reps[full_name.lower()].Lifetime_Total > total: continue
         clean_ID = str(row['ID']).replace(' ', '').strip()
         state = str(row['State']).strip()
         email = str(row['Pol Email']).strip()
         territory = str(row['Territory']).strip()
-        AE = ""
-        #Sets central to East region
+        AE = ''
+        #Sets central to East region and assigns AE accordingly
         if territory == 'Central':
-            territory = 'East'
+            territory = 'East' 
             AE = 'Rob Hunt'
-        elif territory == 'East':
-            AE = 'Rob Hunt'
-        elif territory == 'West':
-            AE = 'MeiWah Wong'
+        elif territory == 'East': AE = 'Rob Hunt'
+        elif territory == 'West': AE = 'MeiWah Wong'
+        reps[full_name.lower()] = Representatives(full_name, clean_ID, state, email, AE, territory, total)
+        nameToID[full_name.lower()] = clean_ID
             
-        reps[clean_ID] = Representatives(full_name, clean_ID, state, email, AE, territory)
-
-
-load_reps_from_csv()
+def attribute_accounts():
+    global reps
+    
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    #sheet_name = input('Enter sheet name (it should be stored in the same directory as this script. Give the name and filetype, such as 12-25.xlsx): ')
+    file_path = os.path.join(base_dir, 'ModelProvider_AUM_RNC_DEC2025_Pivot.xlsx')
+    df = pd.read_excel(file_path, sheet_name='Account-Rep Details', header=0)
+    df.columns = df.columns.str.strip()
+    
+    for _, row in df.iterrows():
+        if str(row['Rep Name']).lower() in reps:
+            reps[row['Rep Name'].lower()].add_account(row['Total Assets'])
+    
+def final_processing():
+    global reps
+    for rep in reps:
+        #assign ranking
+        if reps[rep].Sum_of_Total_Assets == 0: continue
+        elif reps[rep].Sum_of_Total_Assets < 250000: reps[rep].Ranking = 'C'
+        elif reps[rep].Sum_of_Total_Assets < 1000000: reps[rep].Ranking = 'B'
+        elif reps[rep].Sum_of_Total_Assets < 2000000: reps[rep].Ranking = 'BB'
+        elif reps[rep].Sum_of_Total_Assets < 5000000: reps[rep].Ranking = 'A'
+        elif reps[rep].Sum_of_Total_Assets < 10000000: reps[rep].Ranking = 'AA'
+        else: reps[rep].Ranking = 'AAA'
+        
+        
+load_reps_from_xlsx()
+attribute_accounts()
 print(f'loaded {len(reps)} reps')
-print(reps['60916'])
+while True:
+    toPrint = input('Type Advisor name: ')
+    if toPrint.lower() in reps:
+        print(reps[toPrint.lower()])
+    else:
+        print('Not found')
